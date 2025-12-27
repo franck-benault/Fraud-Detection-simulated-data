fc31b8e3d759e31b044e5fc88d74e48e1e316702
franck-benault@orange.fr <franck-benault@orange.fr>
2025-12-27 19:07:38 +0100

    [SAMPLING] Complete random undersampling for descision tree

--- scripts/datamanagement.py
+++ scripts/datamanagement.py
@@ -52,4 +52,22 @@ def show_confusion_matrix(y_test,y_pred,title):
     cm.style.format("{:20}")
     plt.title(title, fontsize=14)
     plt.show()
+
+
+def plt_train_test(range, tabf1Train,trainLabel="f1 Train",tabf1Test=[], testLabel="f1 test"):
+    fig = plt.figure(figsize=(8,6))
+    ax1 = fig.add_subplot()
+
+    ax1.set_ylabel(trainLabel)
+    ax1.plot(range, tabf1Train, color = 'red', label = trainLabel)
+    ax1.legend(loc = 'upper left')
+
+    if(len(tabf1Test)==len(tabf1Train)):
+        ax2 = ax1.twinx()
+        ax2.set_ylabel(testLabel)
+        ax2.plot(range, tabf1Test, color = 'blue', label = testLabel)
+        ax2.legend(loc = 'upper right')
+
+    fig.autofmt_xdate()
+    plt.show()
     
