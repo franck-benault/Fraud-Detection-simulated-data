eec6e8d3c088f13207753cd106f107813cde8b3c
franck-benault@orange.fr <franck-benault@orange.fr>
2025-12-28 21:31:58 +0100

    [RESULT] add learning time

--- scripts/result.py
+++ scripts/result.py
@@ -30,3 +30,31 @@ def update_performance_result(package, name,hyperparameters, f1):
     save_performance_result(data)
 
 
+
+def load_time_response_result():
+    usecols = ['Package','Name','Hyperparameters','Learning time']
+    timeResponse = pd.read_csv("../data/results/timeResponse.csv", usecols=usecols)
+    timeResponse=timeResponse.sort_values(["Package", "Name","Hyperparameters"])
+    return timeResponse
+
+def save_time_response_result(timeResponse): 
+    timeResponse.to_csv('../data/results/timeResponse.csv', index=False) 
+
+def update_time_response_result(package, name,hyperparameters, learningTime):
+    #learningTime=int(learningTime)
+    learningTime=round(learningTime,1)
+
+    timeResponsePandas = load_time_response_result()
+    
+    res=timeResponsePandas[(timeResponsePandas['Package']==package) 
+        & (timeResponsePandas['Name']==name)
+        & (timeResponsePandas['Hyperparameters']==hyperparameters)]
+    #print(res.shape[0])
+    if (res.shape[0]>0):
+        index=res.index[0]
+        #print('trace')
+        timeResponsePandas.loc[index, 'Learning time']=learningTime
+    else:
+        timeResponsePandas=pd.concat([pd.DataFrame([[package,name,hyperparameters,learningTime]], columns=timeResponsePandas.columns), timeResponsePandas], ignore_index=True)
+
+    save_time_response_result(timeResponsePandas)
