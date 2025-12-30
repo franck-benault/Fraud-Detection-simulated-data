de79ae42dbd9b765914144c105acbf42ecec8396
franck-benault@orange.fr <franck-benault@orange.fr>
2025-12-30 21:26:49 +0100

    [GRAPH] Add prediction diagram

--- scripts/graph.py
+++ scripts/graph.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd 
+import numpy as np
 
 
 
@@ -15,3 +16,20 @@ def show_importance(modelClf,predictors):
         plt.show() 
     else:
         print("No feature importance")
+
+
+def show_prediction_graph(modelClf, x_test,y_test):
+    prediction=modelClf.predict_proba(x_test)[:,1]
+    plt.figure(figsize=(10,5))
+    list =np.array([])
+    list0=prediction[y_test==0]
+    list1=prediction[y_test==1]
+    plt.hist(list0, bins=20, label='Negatives',alpha=0.5)
+    for i in np.arange(0,len(list0)/len(list1)): 
+        list= np.append(list,list1)
+
+    plt.hist(list, bins=20, label='Positives', alpha=0.5, color='r')
+    plt.xlabel('Probability of being Positive Class', fontsize=25)
+    plt.legend(fontsize=15)
+    plt.tick_params(axis='both', labelsize=25, pad=5)
+    plt.show() 
