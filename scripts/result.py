529ac4ac623f6f46e939213978c65c1e25e6ec5d
franck-benault@orange.fr <franck-benault@orange.fr>
2026-01-01 11:15:23 +0100

    [RESULT] Add diff F1Test and F1Training

--- scripts/result.py
+++ scripts/result.py
@@ -30,7 +30,7 @@ def update_performance_result(package, name,hyperparameters, f1):
     save_performance_result(data)
 
 
-
+##-------------------------
 def load_time_response_result():
     usecols = ['Package','Name','Hyperparameters','Learning time']
     timeResponse = pd.read_csv("../data/results/timeResponse.csv", usecols=usecols)
@@ -58,3 +58,36 @@ def update_time_response_result(package, name,hyperparameters, learningTime):
         timeResponsePandas=pd.concat([pd.DataFrame([[package,name,hyperparameters,learningTime]], columns=timeResponsePandas.columns), timeResponsePandas], ignore_index=True)
 
     save_time_response_result(timeResponsePandas)
+
+##-------------------
+def load_learning_test_result():
+    usecols = ['Package','Name','Hyperparameters','diff','F1Learning','F1Test']
+    timeResponse = pd.read_csv("../data/results/learnngTest.csv", usecols=usecols)
+    timeResponse=timeResponse.sort_values(["Package", "Name","Hyperparameters"])
+    return timeResponse
+
+def save_learning_test_result(timeResponse): 
+    timeResponse.to_csv('../data/results/learnngTest.csv', index=False) 
+
+def update_learning_test_result(package, name, hyperparameters, F1Learning, F1Test):
+    #learningTime=int(learningTime)
+    F1Learning=round(F1Learning,3)
+    F1Test=round(F1Test,3)
+    diff=F1Learning-F1Test
+
+    timeResponsePandas = load_learning_test_result()
+    
+    res=timeResponsePandas[(timeResponsePandas['Package']==package) 
+        & (timeResponsePandas['Name']==name)
+        & (timeResponsePandas['Hyperparameters']==hyperparameters)]
+    #print(res.shape[0])
+    if (res.shape[0]>0):
+        index=res.index[0]
+        #print('trace')
+        timeResponsePandas.loc[index, 'diff']=diff
+        timeResponsePandas.loc[index, 'F1Learning']=F1Learning
+        timeResponsePandas.loc[index, 'F1Test']=F1Test
+    else:
+        timeResponsePandas=pd.concat([pd.DataFrame([[package,name,hyperparameters,F1Learning,F1Test,diff]], columns=timeResponsePandas.columns), timeResponsePandas], ignore_index=True)
+
+    save_learning_test_result(timeResponsePandas)
