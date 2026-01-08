c48a070625aa96cb3cdbe45ec450c5b09894024d
franck-benault@orange.fr <franck-benault@orange.fr>
2026-01-08 10:16:35 +0100

    [GRAPH] Complete grafic library

--- scripts/graph.py
+++ scripts/graph.py
@@ -33,3 +33,20 @@ def show_prediction_graph(modelClf, x_test,y_test):
     plt.legend(fontsize=15)
     plt.tick_params(axis='both', labelsize=25, pad=5)
     plt.show() 
+
+def plt_train_test(range, tabf1Train,trainLabel="f1 Train",tabf1Test=[], testLabel="f1 test"):
+    fig = plt.figure(figsize=(8,6))
+    ax1 = fig.add_subplot()
+
+    ax1.set_ylabel(trainLabel)
+    ax1.plot(range, tabf1Train, color = 'red', label = trainLabel)
+    ax1.legend(loc = 'upper left')
+
+    if(len(tabf1Test)==len(tabf1Train)):
+        ax2 = ax1.twinx()
+        ax2.set_ylabel(testLabel)
+        ax2.plot(range, tabf1Test, color = 'blue', label = testLabel)
+        ax2.legend(loc = 'upper right')
+
+    fig.autofmt_xdate()
+    plt.show()
